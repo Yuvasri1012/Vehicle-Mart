ed87385c54607d6fd9757bb6aa0102d03411d4af
Yuvasri1012 <yuvasrir120@gmail.com>
2026-04-10 14:58:22 +0530

    Database Settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv 
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +31,7 @@ SECRET_KEY = 'django-insecure-u2i1djk_)yilu8f^%qd3$84oixc^v0j7dtqyj=$+^68iy@3na@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -79,11 +83,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": dj_database_url.config( 
+        default=os.getenv("DATABASE_URL"), 
+        conn_max_age=600,
+        ssl_require=True # Render PostgreSQL requires SSL
+)
 }
+
 
 
 # Password validation
