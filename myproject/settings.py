515661a9157d072bae93987ae3912c8fe5759eac
Yuvasri1012 <yuvasrir120@gmail.com>
2026-04-11 14:32:09 +0530

    ssl removed

--- myproject/settings.py
+++ myproject/settings.py
@@ -87,7 +87,6 @@ DATABASES = {
 "default": dj_database_url.config( 
         default=os.getenv("DATABASE_URL"), 
         conn_max_age=600,
-        ssl_require=True # Render PostgreSQL requires SSL
 )
 }
 
